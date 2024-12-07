cd5e5183596061a7294228a8cdc2f7180187513d
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-12-07 19:03:12 +0600

    Set up POST route for creating new campaign

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
+// http://localhost:5000
 const port = process.env.PORT || 5000;
 
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
 // mongodb database
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uulp6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ttbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,6 +25,9 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+
+
 async function run() {
     try {
         // // Connect the client to the server	(optional starting in v4.7)
@@ -25,23 +35,27 @@ async function run() {
         // // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+        app.post('/campaigns', async (req, res) => {
+            const newCampaign = req.body;
+            console.log(newCampaign);
+        })
     } finally {
-        // // Ensures that the client will close when you finish/error
-        // await client.close();
+        //   // Ensures that the client will close when you finish/error
+        //   await client.close();
     }
 }
 run().catch(console.dir);
 
 
-// middleware
-app.use(express.json());
-app.use(cors());
 
-// routes
+// routing
 app.get("/", (req, res) => {
     res.send("welcome to home page");
 })
 
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 })
