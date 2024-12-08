ffd2f69ef2ec5ae7ac60423993b72be02dfa2206
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-12-08 10:35:30 +0600

    Implement filtering of campaigns based on deadline date

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 
 // mongodb database
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ttbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uulp6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -38,10 +38,20 @@ async function run() {
 
         const campaignCollection = client.db("campaignDB").collection("campaign");
 
+        // AddNewCampaign section of client side
         app.post('/campaign', async (req, res) => {
             const newCampaign = req.body;
 
             const result = await campaignCollection.insertOne(newCampaign);
+            res.send(result);
+        })
+
+        // RunningCampaign & Home sections of client side
+        app.get('/runningCampaign', async (req, res) => {
+            const currentDate = new Date();
+            const formattedDate = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}-${String(currentDate.getDate()).padStart(2, '0')}`;
+
+            const result = await campaignCollection.find({ deadline: { $gt: formattedDate } }).limit(6).toArray();
             res.send(result);
         })
     } finally {
